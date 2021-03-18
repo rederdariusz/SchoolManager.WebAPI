2ad80e6e6f1dde7d4eed0a9491efdbee026b7806
Dariusz Reder <rederdariusz@gmail.com>
2021-03-18 18:32:14 +0100

    Added dto mapping to get all classes query.

--- SchoolManager.Application/Classes/Queries/GetAllClasses/GetAllClassesQuery.cs
+++ SchoolManager.Application/Classes/Queries/GetAllClasses/GetAllClassesQuery.cs
@@ -1,6 +1,9 @@
-﻿using MediatR;
+﻿using Mapster;
+using MapsterMapper;
+using MediatR;
 using Microsoft.EntityFrameworkCore;
 using SchoolManager.Application.Common.Interfaces;
+using SchoolManager.Application.Dto;
 using SchoolManager.Domain.Entities;
 using System;
 using System.Collections.Generic;
@@ -11,24 +14,27 @@ using System.Threading.Tasks;
 
 namespace SchoolManager.Application.Classes.Queries.GetAllClasses
 {
-    public class GetAllClassesQuery : IRequest<IEnumerable<Class>>
+    public class GetAllClassesQuery : IRequest<IEnumerable<ClassDto>>
     {
 
     }
 
-    public class GetAllClassesQueryHandler : IRequestHandler<GetAllClassesQuery, IEnumerable<Class>>
+    public class GetAllClassesQueryHandler : IRequestHandler<GetAllClassesQuery, IEnumerable<ClassDto>>
     {
         private readonly ISchoolDbContext _context;
+        private readonly IMapper _mapper;
 
-        public GetAllClassesQueryHandler(ISchoolDbContext context)
+        public GetAllClassesQueryHandler(ISchoolDbContext context, IMapper mapper)
         {
             _context = context;
+            _mapper = mapper;
         }
-        public async Task<IEnumerable<Class>> Handle(GetAllClassesQuery request, CancellationToken cancellationToken)
+        public async Task<IEnumerable<ClassDto>> Handle(GetAllClassesQuery request, CancellationToken cancellationToken)
         {
             return await _context.Classes
                 .Include(c => c.Teacher)
                 .Include(c => c.Students)
+                .ProjectToType<ClassDto>(_mapper.Config)
                 .ToListAsync();
 
         }
