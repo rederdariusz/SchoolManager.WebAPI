62630560080f0923da298093e77a594a053da03e
Dariusz Reder <rederdariusz@gmail.com>
2021-03-22 18:52:23 +0100

    Added documentation for students and teacher contoller

--- SchoolManager.API/Controllers/StudentsController.cs
+++ SchoolManager.API/Controllers/StudentsController.cs
@@ -23,17 +23,56 @@ namespace SchoolManager.API.Controllers
         {
             _mediator = mediator;
         }
-        
+        /// <summary>
+        ///     Returns all students from class with given id
+        /// </summary>
+        /// <param name="classId"></param>
+        /// <response code="200">Success</response>
+        /// <response code="404">Class not found</response>
+        /// <response code="500">Server Error</response>
         [HttpGet]
         public async Task<IActionResult> GetAllStudents(int classId)
         {
             return Ok(await _mediator.Send(new GetAllStudentsQuery{ ClassId = classId }));
         }
+
+        /// <summary>
+        ///     Returns student with given studentId from class with given classId
+        /// </summary>
+        /// <param name="classId"></param>
+        /// <param name="studentId"></param>
+        /// <response code="200">Success</response>
+        /// <response code="404">Class or student not found</response>
+        /// <response code="500">Server Error</response>
         [HttpGet("{studentId}")]
         public async Task<IActionResult> GetStudentById(int classId, int studentId)
         {
             return Ok(await _mediator.Send(new GetStudentByIdQuery { ClassId = classId, StudentId = studentId }));
         }
+
+        /// <summary>
+        ///     Creates student in class with given id.
+        /// </summary>
+        /// <param name="command"></param>
+        /// <param name="classId"></param>
+        /// <remarks>
+        ///   Sample request:
+        ///
+        ///     POST /api/classes/{classId}/students
+        ///     {
+        ///         "firstName": "Bobby",
+        ///         "lastName: "Fischer",
+        ///         "pesel: "012345678910",
+        ///         "dateOfBirth": "1966-10-02",
+        ///         "phoneNumber": "501505101         
+        ///     }
+        ///     
+        /// ClassID is not required
+        /// </remarks>
+        /// <response code="201">Success created</response>
+        /// <response code="400">Bad Request</response>
+        /// <response code="404">Class not found</response>
+        /// <response code="500">Server Error</response>
         [HttpPost]
         public async Task<IActionResult> CreateStudent(int classId,[FromBody] CreateStudentCommand command)
         {
@@ -41,6 +80,15 @@ namespace SchoolManager.API.Controllers
             var studentId = await _mediator.Send(command);
             return Created($"api/classes/{classId}/students/{studentId}", null);
         }
+
+        /// <summary>
+        ///     Deletes student with given studentId in class with given classId.
+        /// </summary>
+        /// <param name="studentId"></param>
+        /// <param name="classId"></param>
+        /// <response code="204">Success deleted</response>
+        /// <response code="404">Class or student not found</response>
+        /// <response code="500">Server Error</response>
         [HttpDelete("{studentId}")]
         public async Task<IActionResult> DeleteStudent(int classId, int studentId)
         {
@@ -48,6 +96,30 @@ namespace SchoolManager.API.Controllers
             return NoContent();
         }
 
+        /// <summary>
+        ///     Updates student with given studentId in class with given classId.
+        /// </summary>
+        /// <param name="studentId"></param>
+        /// <param name="classId"></param>
+        /// <param name="command"></param>
+        /// <remarks>
+        ///   Sample request:
+        ///
+        ///     POST /api/classes/{classId}/students/{studentId}
+        ///     {
+        ///         "firstName": "Bobby",
+        ///         "lastName: "Fischer",
+        ///         "pesel: "012345678910",
+        ///         "dateOfBirth": "1966-10-02",
+        ///         "phoneNumber": "501505101         
+        ///     }
+        ///     
+        /// ClassID is not required
+        /// </remarks>
+        /// <response code="200">Success Updated</response>
+        /// <response code="400">Bad Request</response>
+        /// <response code="404">Class or student not found</response>
+        /// <response code="500">Server Error</response>
         [HttpPut("{studentId}")]
         public async Task<IActionResult> UpdateStudent(int classId, int studentId, [FromBody]UpdateStudentCommand command)
         {
--- SchoolManager.API/Controllers/TeacherController.cs
+++ SchoolManager.API/Controllers/TeacherController.cs
@@ -23,11 +23,43 @@ namespace SchoolManager.API.Controllers
             _mediator = mediator;
         }
 
+        /// <summary>
+        ///     Returns teacher from class with given id.
+        /// </summary>
+        /// 
+        /// <param name="classId"></param>
+        /// <response code="200">Success</response>
+        /// <response code="404">Class not found</response>
+        /// <response code="500">Server Error</response>
         [HttpGet]
         public async Task<IActionResult> GetTeacher(int classId)
         {
             return Ok(await _mediator.Send(new GetTeacherQuery { ClassId = classId }));
         }
+
+        /// <summary>
+        ///     Creates teacher in class with given id.
+        /// </summary>
+        /// <param name="command"></param>
+        /// <param name="classId"></param>
+        /// <remarks>
+        ///   Sample request:
+        ///
+        ///     POST /api/classes/{classId}/teacher
+        ///     {
+        ///         "firstName": "Magnus",
+        ///         "lastName: "Carlsen",
+        ///         "pesel: "012345678910",
+        ///         "dateOfBirth": "1980-10-02",
+        ///         "phoneNumber": "501505101 "        
+        ///     }
+        ///     
+        /// ClassID is not required
+        /// </remarks>
+        /// <response code="201">Success created</response>
+        /// <response code="400">Bad Request</response>
+        /// <response code="404">Class not found</response>
+        /// <response code="500">Server Error</response>
         [HttpPost]
         public async Task<IActionResult> CreateTeacher(int classId, [FromBody] CreateTeacherCommand command)
         {
@@ -35,14 +67,46 @@ namespace SchoolManager.API.Controllers
             await _mediator.Send(command);
             return Created($"api/classes/{classId}/teacher", null);
         }
+
+        /// <summary>
+        ///     Deletes teacher in class with given id.
+        /// </summary>
+        /// <param name="classId"></param>
+        /// <response code="204">Success deleted</response>
+        /// <response code="404">Class or teacher not found</response>
+        /// <response code="500">Server Error</response>
         [HttpDelete]
-        public async Task<IActionResult> DeleteStudent(int classId)
+        public async Task<IActionResult> DeleteTeacher(int classId)
         {
             await _mediator.Send(new DeleteTeacherCommand { ClassId = classId});
             return NoContent();
         }
+
+        /// <summary>
+        ///     Updates teacher in class with given id.
+        /// </summary>
+        /// <param name="command"></param>
+        /// <param name="classId"></param>
+        /// <remarks>
+        ///   Sample request:
+        ///
+        ///     POST /api/classes/{classId}/students
+        ///     {
+        ///         "firstName": "Bobby",
+        ///         "lastName: "Fischer",
+        ///         "pesel: "012345678910",
+        ///         "dateOfBirth": "1966-10-02",
+        ///         "phoneNumber": "501505101         
+        ///     }
+        ///     
+        /// ClassID is not required
+        /// </remarks>
+        /// <response code="200">Success updated</response>
+        /// <response code="400">Bad Request</response>
+        /// <response code="404">Class or teacher not found</response>
+        /// <response code="500">Server Error</response>
         [HttpPut]
-        public async Task<IActionResult> UpdateStudent(int classId, [FromBody] UpdateTeacherCommand command)
+        public async Task<IActionResult> UpdateTeacher(int classId, [FromBody] UpdateTeacherCommand command)
         {
             command.ClassId = classId;
             await _mediator.Send(command);
